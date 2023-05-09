c24f44a7bb60ff5777a2ee9481fce57e9ac1241a
samuraidays <1samuraiboy@gmail.com>
2023-05-09 17:45:56 +0800

    add Mongoose

--- app/app.js
+++ app/app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const taskRoute = require("./routes/tasks");
-const port = 8100;
-
-app.use("/api/v1/tasks", taskRoute);
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const PORT = 8100;
 
 app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`サーバ起動 on port ${port}`)
-  })
+// ルーティング
+app.use("/api/v1/tasks", taskRoute);
+
+// データベース接続
+const start  = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        // サーバ起動
+        app.listen(PORT, console.log("サーバが起動しました"));
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+start();
