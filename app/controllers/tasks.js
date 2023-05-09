871c391b63e0e3a7c44d928d96ced7bfa2a24d1e
samuraidays <1samuraiboy@gmail.com>
2023-05-09 18:43:46 +0800

    add createTask

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,10 +1,18 @@
+const Task = require("../models/Task");
+
 const getAllTasks = (req, res) => {
     res.send('タスクをすべて取得しました')
 }
 
-const createTask = (req, res) => {
-    res.send('タスクを新規作成しました')
-}
+const createTask = async (req, res) => {
+    try {
+        const createTask = await Task.create(req.body);
+        res.status(200).json(createTask);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+
+};
 
 const getSingleTask = (req, res) => {
     res.send('タスクを取得しました')
